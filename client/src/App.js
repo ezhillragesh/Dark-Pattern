6f40dcaff156a7890918b499d6f8986836cc2925
Ragesh <rag3shx@gmail.com>
2024-03-17 10:13:12 +0000

    minor change

--- client/src/App.js
+++ client/src/App.js
@@ -25,7 +25,7 @@ const requestOptions = {
   redirect: "follow"
 };
   
-fetch("https://supreme-spork-59wv97rppg9f656-5000.app.github.dev/process_link", requestOptions)
+fetch("https://dark-pattern.onrender.com/process_link", requestOptions)
   .then(response => response.json())
   .then(data => {
     const summaryText = data.message.Summary;
